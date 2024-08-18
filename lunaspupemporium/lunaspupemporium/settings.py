b5aa3a641da0cd008fa9787b341ccedd7aef6987
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-18 12:57:56 +0000

    add allowed hosts for local testing

--- lunaspupemporium/lunaspupemporium/settings.py
+++ lunaspupemporium/lunaspupemporium/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',
+                 'localhost',]
 
 
 # Application definition
