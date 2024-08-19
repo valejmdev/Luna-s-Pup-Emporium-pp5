e15ae3ff195b18c6681b7b7b4c8c807e185b19d6
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-19 17:53:14 +0000

    add models to admin of store logic

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description')
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'category')
+    search_fields = ('name', 'category__name')
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'image')
+    search_fields = ('product__name',)
