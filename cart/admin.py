0f7155bae1ff66055a392ee252e80bc21b530c91
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-19 17:38:59 +0000

    create cart app and added models to admin

---
+++ cart/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Cart, CartItem
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at')
+    search_fields = ('user__username',)
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
+    search_fields = ('cart__user__username', 'product__name')
