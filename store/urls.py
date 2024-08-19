0abb930f27f8a3da7c2f0b55bde317b82a470eea
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-19 17:53:28 +0000

    add urls to store app

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Home page
+    path('', views.index, name='index'),  
+    path('products/', views.product_list, name='product_list'),  
+    path('products/<slug:category_slug>/', views.product_list_by_category, name='product_list_by_category'),  
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),  
+    path('faq/', views.faq, name='faq'),  
+    path('about/', views.about_us, name='about_us'),  
 ]
