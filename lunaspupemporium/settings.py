e56d865760c3f2439f4bf3058cf6348abdd85a68
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-19 12:43:29 +0000

    edit static files location due to error in display

--- lunaspupemporium/settings.py
+++ lunaspupemporium/settings.py
@@ -166,6 +166,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
