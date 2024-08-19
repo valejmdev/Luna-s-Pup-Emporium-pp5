637bd9acb0d283a9eda4e414f42aa30f09e75da9
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-19 17:53:55 +0000

    add models for store app

--- store/models.py
+++ store/models.py
@@ -1,4 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class MyModel(models.Model):
-    image = models.ImageField(upload_to='images/')
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')  # Main product image
+    stock = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
