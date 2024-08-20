1221e07352c5aaf48702e3624eb12bb7810b98d0
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-20 12:59:11 +0000

    edit checkout urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'checkout'
+
 urlpatterns = [
     path('', views.checkout, name='checkout'),  
     path('order-confirmation/', views.order_confirmation, name='order_confirmation'),  
