dd5f9281df7860bba30f964c207e5413337b1eda
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-20 16:04:39 +0000

    edit cloudinary changes in models

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -13,7 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')  # Main product image
+    featured_image = CloudinaryField('image', default='placeholder')  
     stock = models.PositiveIntegerField()
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
 
@@ -22,7 +23,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/')
+    featured_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return f"Image for {self.product.name}"
