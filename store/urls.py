67a551be9ff0d54d4d383f0bd7e680c158021278
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-20 13:00:07 +0000

    add products pages urls

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'store' 
+
 urlpatterns = [
-    path('', views.index, name='index'),  
-    path('products/', views.product_list, name='product_list'),  
-    path('products/<slug:category_slug>/', views.product_list_by_category, name='product_list_by_category'),  
-    path('product/<slug:slug>/', views.product_detail, name='product_detail'),  
-    path('faq/', views.faq, name='faq'),  
-    path('about/', views.about_us, name='about_us'),  
+    path('', views.index, name='index'),
+    path('all-products/', views.all_products, name='all_products'),
+    path('category/<slug:category_slug>/', views.product_list, name='product_list'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('faq/', views.faq, name='faq'),
+    path('about/', views.about_us, name='about_us'),
+    path('contact/', views.contact_us, name='contact_us'),
 ]
