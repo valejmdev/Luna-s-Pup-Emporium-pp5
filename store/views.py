d42a3bdf063ae5941ea2374d6a911f2ad2a93977
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-20 13:00:38 +0000

    edit views for products lists and details

--- store/views.py
+++ store/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+# Product List View: Displays all products in a specific category
+def product_list(request, category_slug=None):
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = category.products.all()  # Using related_name='products'
+    else:
+        products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products, 'category_slug': category_slug})
+
+# Product Detail View: Displays details of a single product
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    images = product.images.all()  # Fetching related product images
+    return render(request, 'products/product_detail.html', {'product': product, 'images': images})
+
+# View to display all products
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'products/all_products.html', {'products': products})
 
 def index(request):
     return render(request, 'store/index.html')
-
-def product_list(request):
-    return render(request, 'store/product_list.html')
-
-def product_list_by_category(request, category_slug):
-    return render(request, 'store/product_list_by_category.html')
-
-def product_detail(request, slug):
-    return render(request, 'store/product_detail.html')
 
 def faq(request):
     return render(request, 'store/faq.html')
