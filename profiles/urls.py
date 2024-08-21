dc94150957167d17a429b4818487ab0a7047aaac
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-21 15:30:22 +0000

    edit urls for user navigation

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,8 +6,9 @@ app_name = 'profile'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('profile/', views.profile, name='profile'),
+    path('<str:username>/', views.profile, name='profile'),
     path('logout/', views.logout, name='logout'),
     path('newsletter/', views.newsletter, name='newsletter'),
     path('terms_conditions/', views.terms_conditions, name='terms_conditions'),
+
 ]
