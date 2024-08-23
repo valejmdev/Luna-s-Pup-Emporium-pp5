c8e7e25edfec864744b0ea966751b1679c5bfd59
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-23 13:28:45 +0000

    edit forms after all auth implement

--- profiles/forms.py
+++ profiles/forms.py
@@ -8,16 +8,12 @@ class CustomSignupForm(SignupForm):
     phone_number = forms.CharField(max_length=15, required=False)
 
     def save(self, request):
-        # Save the base SignupForm first (which handles the creation of the User)
         user = super(CustomSignupForm, self).save(request)
-
-        # Create the UserProfile for the user
         UserProfile.objects.create(
             user=user,
             address=self.cleaned_data.get('address'),
             phone_number=self.cleaned_data.get('phone_number')
         )
-
         return user
 
 class UserUpdateForm(forms.ModelForm):
@@ -32,7 +28,6 @@ class UserUpdateForm(forms.ModelForm):
         user = super().save(commit=False)
         if commit:
             user.save()
-            # Ensure the user profile is updated with new data
             user_profile, created = UserProfile.objects.get_or_create(user=user)
             user_profile.address = self.cleaned_data.get('address')
             user_profile.phone_number = self.cleaned_data.get('phone_number')
