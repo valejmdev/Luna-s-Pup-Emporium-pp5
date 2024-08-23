086073a4771949edbbb87e681f0ac95dbd6955f6
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-23 13:09:23 +0000

    edit views to use all auth

--- profiles/views.py
+++ profiles/views.py
@@ -4,22 +4,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile  
 from .forms import UserUpdateForm
-from django.contrib.auth import logout as auth_logout
-
-def register(request):
-    """
-    Handle user registration.
-    """
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Thank you for creating an account, {username}.')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'profiles/register.html', {'form': form})
 
 @login_required
 def profile(request, username):
@@ -44,7 +28,7 @@ def profile(request, username):
             return redirect('home')
 
     else:
-        # Here, ensure that the form is populated with existing data
+        # Ensure the form is populated with existing data
         u_form = UserUpdateForm(
             instance=user,
             initial={
@@ -62,29 +46,3 @@ def profile(request, username):
     }
 
     return render(request, 'profiles/profile.html', context)
-
-def login(request):
-    """
-    Handle user login.
-    """
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            auth_login(request, user)
-            messages.success(request, 'You are now logged in.')
-            return redirect('profile')
-    else:
-        form = AuthenticationForm()
-
-    return render(request, 'profiles/login.html', {'form': form})
-
-def logout(request):
-    auth_logout(request)
-    return redirect('store/index.html')
-
-def newsletter(request):
-    return render(request, 'profiles/newsletter.html')
-
-def terms_conditions(request):
-    return render(request, 'profiles/terms_conditions.html')
