60830d6c8e821a1fa108f4bc54130daf314d630a
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-23 13:05:46 +0000

    edit forms for django all auth

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,25 +1,23 @@
+from allauth.account.forms import SignupForm
 from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfile
 
-class UserRegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+class CustomSignupForm(SignupForm):
     address = forms.CharField(max_length=255, required=False)
     phone_number = forms.CharField(max_length=15, required=False)
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'address', 'phone_number']
+    def save(self, request):
+        # Save the base SignupForm first (which handles the creation of the User)
+        user = super(CustomSignupForm, self).save(request)
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if commit:
-            user.save()
-            UserProfile.objects.create(
-                user=user,
-                address=self.cleaned_data.get('address'),
-                phone_number=self.cleaned_data.get('phone_number')
-            )
+        # Create the UserProfile for the user
+        UserProfile.objects.create(
+            user=user,
+            address=self.cleaned_data.get('address'),
+            phone_number=self.cleaned_data.get('phone_number')
+        )
+
         return user
 
 class UserUpdateForm(forms.ModelForm):
@@ -34,8 +32,8 @@ class UserUpdateForm(forms.ModelForm):
         user = super().save(commit=False)
         if commit:
             user.save()
-            # Corrected the reference from user.profile to user.userprofile
-            user_profile = UserProfile.objects.get(user=user)
+            # Ensure the user profile is updated with new data
+            user_profile, created = UserProfile.objects.get_or_create(user=user)
             user_profile.address = self.cleaned_data.get('address')
             user_profile.phone_number = self.cleaned_data.get('phone_number')
             user_profile.save()
