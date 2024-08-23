9f3145f8bfea5cd82f02c60d84b6f330171f8a67
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-23 13:09:05 +0000

    edit profiles urls for all auth

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'profile'
+app_name = 'profiles'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
     path('<str:username>/', views.profile, name='profile'),
-    path('logout/', views.logout, name='logout'),
     path('newsletter/', views.newsletter, name='newsletter'),
     path('terms_conditions/', views.terms_conditions, name='terms_conditions'),
-
 ]
