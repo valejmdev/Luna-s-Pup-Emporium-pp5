25adeb39904ce88664a942cea51cd5b216f0c350
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-28 15:49:50 +0000

    edit views for easier cart model

--- cart/views.py
+++ cart/views.py
@@ -1,27 +1,30 @@
-from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Cart, CartItem
 from store.models import Product
+from .models import Cart, CartItem
 
-def cart_detail(request):
-    cart, created = Cart.objects.get_or_create(user=request.user)
-    return render(request, 'cart/cart_detail.html', {'cart': cart})
+def get_or_create_cart(user):
+    cart, created = Cart.objects.get_or_create(user=user)
+    return cart
 
 def cart_add(request, product_id):
-    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart = get_or_create_cart(request.user)
     product = get_object_or_404(Product, id=product_id)
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
     if not created:
         cart_item.quantity += 1
-        cart_item.save()
-        messages.success(request, f'{product.name} quantity updated in your cart.')
-    else:
-        messages.success(request, f'{product.name} added to your cart.')
-    return redirect('cart_detail')
+    cart_item.save()
+    return redirect('cart:cart_detail')
 
 def cart_remove(request, product_id):
-    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart = get_or_create_cart(request.user)
     product = get_object_or_404(Product, id=product_id)
-    CartItem.objects.filter(cart=cart, product=product).delete()
-    messages.success(request, f'{product.name} removed from your cart.')
-    return redirect('cart_detail')
+    cart_item = CartItem.objects.filter(cart=cart, product=product).first()
+    if cart_item:
+        cart_item.delete()
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = get_or_create_cart(request.user)
+    cart_items = CartItem.objects.filter(cart=cart)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'cart/cart_detail.html', {'cart_items': cart_items, 'total_price': total_price})
