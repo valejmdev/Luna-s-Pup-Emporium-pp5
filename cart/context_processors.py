c67ecc69f438d5bd30a16d51d45985fbdb858c18
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-29 16:19:55 +0000

    add cart_proccessor to context processors

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,7 +1,14 @@
-from .models import Cart
+from cart.views import get_or_create_cart
+from .models import Cart, CartItem
 
 def cart_item_count(request):
     if request.user.is_authenticated:
         cart = Cart.objects.get(user=request.user)
         return {'cart_items_count': cart.items.count()}
     return {'cart_items_count': 0}
+
+
+def cart_processor(request):
+    cart = get_or_create_cart(request.user)
+    cart_items = CartItem.objects.filter(cart=cart)
+    return {'cart_items': cart_items}
