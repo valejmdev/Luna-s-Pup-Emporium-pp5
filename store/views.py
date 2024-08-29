4f9fe4692c0ea7dbd265c2430654d8ac2dab2b15
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-29 16:23:38 +0000

    edit product views to reflect add to cart changes

--- store/views.py
+++ store/views.py
@@ -5,13 +5,13 @@ from django.db.models import Q, Avg
 def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = category.products.all()  # Using related_name='products'
+        products = category.products.all()
     else:
         products = Product.objects.all()
 
     for product in products:
-        product.sale_price = product.get_sale_price()  # Get sale price if applicable
-        product.avg_rating = product.average_rating()  # Get average rating
+        product.sale_price = product.get_sale_price()  # Calculate sale price
+        product.avg_rating = product.average_rating()
 
     return render(request, 'store/product_list.html', {'products': products, 'category_slug': category_slug})
 
@@ -27,8 +27,6 @@ def product_detail(request, product_id):
     return render(request, 'store/product_detail.html', {'product': product, 'images': images})
 
 # View to display all products
-
-
 def all_products(request):
     """A view to show all products, including sorting and search queries"""
     
@@ -48,7 +46,9 @@ def all_products(request):
     return render(request, 'store/all_products.html', context)
 
 def special_offers(request):
-    products_on_sale = Product.objects.filter(is_on_sale=True)
+    products_on_sale = Product.objects.filter(on_sale=True)
+    for product in products_on_sale:
+        product.sale_price = product.get_sale_price()
     return render(request, 'store/special_offers.html', {'products': products_on_sale})
 
 def index(request):
@@ -65,3 +65,8 @@ def contact_us(request):
 
 def terms_conditions(request):
     return render(request, 'store/terms_conditions.html')
+
+def newsletter(request):
+    # Your view logic here
+    return render(request, 'store/newsletter.html')
+
