40c0bd43326b36afae2142ff7b83afcde8b01fd2
valejmdev Müller <valentinjmueller055@gmail.com>
2024-08-29 16:20:34 +0000

    edit models to have quantity and validation

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from store.models import Product
+from django.core.exceptions import ValidationError
 
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -9,10 +10,23 @@ class Cart(models.Model):
     def __str__(self):
         return f"Cart {self.id} for {self.user.username}"
 
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)  # Set default value to avoid None
 
     def __str__(self):
         return f"{self.quantity} of {self.product.name} in Cart {self.cart.id}"
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    def clean(self):
+        if self.quantity is None or self.quantity <= 0:
+            raise ValidationError('Quantity must be greater than zero.')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
