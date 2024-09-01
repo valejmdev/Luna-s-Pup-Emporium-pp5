773bd589f68956634625c851fc5648a4d1cb431e
valejmdev Müller <valentinjmueller055@gmail.com>
2024-09-01 21:17:23 +0000

    create forms.py for payment information

---
+++ checkout/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country', 'county')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'town_or_city': 'Town or City',
+            'postcode': 'Postal Code',
+            'country': 'Country',
+            'county': 'County',
+        }
+
+        for field in self.fields:
+            placeholder = f'{placeholders[field]} *' if self.fields[field].required else placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
