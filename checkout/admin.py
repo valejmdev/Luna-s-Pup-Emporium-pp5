81f46122d5b261c2e31b756417fe2772c711982a
valejmdev Müller <valentinjmueller055@gmail.com>
2024-09-01 21:17:08 +0000

    add edited model to admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderLineItem 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'created_at', 'total', 'payment_status', 'order_status')
-    search_fields = ('user__username',)
+    list_display = ('order_number', 'full_name', 'email', 'phone_number', 'order_total', 'grand_total', 'date')
+    search_fields = ('order_number', 'full_name', 'email')
 
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('order', 'product', 'quantity')
-    search_fields = ('order__user__username', 'product__name')
+@admin.register(OrderLineItem)
+class OrderLineItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'lineitem_total')
+    search_fields = ('order__order_number', 'product__name')
