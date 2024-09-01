cf1da87bee72dc6da2fd2044cebc85bcb6b4edf5
valejmdev Müller <valentinjmueller055@gmail.com>
2024-09-01 21:20:27 +0000

    add relevant paths for checkout and payment

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'checkout'
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),  
-    path('order-confirmation/', views.order_confirmation, name='order_confirmation'),  
+    path('', views.checkout, name='checkout'),
+    path('webhook/', views.stripe_webhook, name='stripe_webhook'),
+    path('order_confirmation/<order_number>/', views.order_confirmation, name='order_confirmation'),
 ]
