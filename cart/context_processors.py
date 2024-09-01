e8fd390b1460c7c9c624e23d52c48c4b0568dc5b
valejmdev Müller <valentinjmueller055@gmail.com>
2024-09-01 21:13:11 +0000

    edit cart processors due to error in register

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -3,13 +3,16 @@ from .models import Cart, CartItem
 
 def cart_item_count(request):
     if request.user.is_authenticated:
-        cart = Cart.objects.get(user=request.user)
-        return {'cart_items_count': cart.items.count()}
+        try:
+            cart = get_or_create_cart(request.user)
+            return {'cart_items_count': cart.items.count() if cart else 0}
+        except Cart.DoesNotExist:
+            return {'cart_items_count': 0}
     return {'cart_items_count': 0}
 
 
 def cart_processor(request):
-    cart = get_or_create_cart(request.user)
+    cart = get_or_create_cart(request.user) if request.user.is_authenticated else None
     if cart is None:
         cart_items = []
     else:
