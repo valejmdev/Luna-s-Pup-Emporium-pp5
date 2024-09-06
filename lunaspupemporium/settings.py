1a58c748c34d431e8f5c5e96e10ffdc2395c71ae
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-09-06 15:06:08 +0000

    edit location of account templates and fix index

--- lunaspupemporium/settings.py
+++ lunaspupemporium/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',
@@ -84,6 +84,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'profiles', 'templates'),
             os.path.join(BASE_DIR, 'cart', 'templates'),      
             os.path.join(BASE_DIR, 'checkout', 'templates'), 
