ece7da433b24d6ae9a1b9bde325b76b68281e449
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-09-07 01:48:48 +0000

    add welcome email to profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+
+@receiver(post_save, sender=User)
+def send_welcome_email(sender, instance, created, **kwargs):
+    if created:
+        subject = "Welcome to Luna’s Pup Emporium"
+        message = f"Hi {instance.username}, thanks for registering!"
+        email_from = settings.DEFAULT_FROM_EMAIL
+        recipient_list = [instance.email]
+        send_mail(subject, message, email_from, recipient_list)
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
