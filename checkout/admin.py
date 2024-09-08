bb937bbb6837b4f486a2ec0b0de7e55f4395889c
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-09-08 22:08:41 +0000

    edit admin.py to not include orderItem

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
+from .models import Order
 
 
 @admin.register(Order)
@@ -7,9 +7,3 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_number', 'full_name', 'email',
                     'phone_number', 'order_total', 'grand_total', 'date')
     search_fields = ('order_number', 'full_name', 'email')
-
-
-@admin.register(OrderLineItem)
-class OrderLineItemAdmin(admin.ModelAdmin):
-    list_display = ('order', 'product', 'quantity', 'lineitem_total')
-    search_fields = ('order__order_number', 'product__name')
